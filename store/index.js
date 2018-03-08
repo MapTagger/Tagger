e5d372e0342607b79adc7d6ce7cf27349183a23c
Philip Gingerich <philipygingerich@gmail.com>
2018-03-08 18:43:37 -0500

    Got user inputs

--- store/index.js
+++ store/index.js
@@ -1,19 +1,24 @@
 import {createStore} from 'redux'
 
 const ADD_REQUEST = 'ADD_REQUEST'
+const CHANGE_INPUT = 'CHANGE_INPUT'
 
 //action creators
 
 export const addCommunication = message => ({type: ADD_REQUEST, message})
+export const changeInput = input => ({type: CHANGE_INPUT, input})
 
 const initialState = {
-    message : ['Initialization']
+    message : ['Initialization'],
+    currentInput : ''
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type){
         case ADD_REQUEST:
             return Object.assign({}, state, {message: [...state.message, action.message]})
+        case CHANGE_INPUT:
+            return Object.assign({}, state, { currentInput: action.input })
         default: return state
     }
 }
