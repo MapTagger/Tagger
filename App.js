15835ed2e7e3e71de039175b3121f081f312e7c1
Philip Gingerich <philipygingerich@gmail.com>
2018-03-09 18:10:55 -0500

    Junk

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Expo from 'expo';
 import  MapView, { Marker } from 'react-native-maps';
 import {StyleSheet, Text, View, TextInput} from 'react-native'
 import {Provider, connect} from 'react-redux'
 import store, {addCommunication} from './store'
 import AddRecommendations from './components/add-recommendations.js'
-import SocketIOClient from 'socket.io-client'
+const io = require('socket.io-client')
+
+const SocketEndpoint = 'localhost:3000';
+
+
 
 
 export default class App extends React.Component {
@@ -12,16 +17,34 @@ export default class App extends React.Component {
   constructor(props){
     super(props)
 
-    //console.log('!!!', window.location.origin)
-    
-    this.socket = SocketIOClient('0')
-    this.socket.on('connect',()=>{
-      console.log('WE CONNECTED TO SERVER')
+    state = {
+      isConnected: false,
+      data: null,
     }
-  )
-
-    console.log('WE ARE HERE')
+    console.log('Hellloooo')
+  
   }
+
+  componentDidMount() {
+
+    console.log('Hellloooo')
+
+    const socket = io(SocketEndpoint, {
+      transports: ['websocket'],
+    });
+    
+    socket.on('connect', () => {
+      console.log('HOLY MOTHER')
+      this.setState({ isConnected: true });
+    });
+
+    socket.on('ping', data => {
+      this.setState(data);
+    });
+  }
+
+
+
   render() {
 
     return (  
